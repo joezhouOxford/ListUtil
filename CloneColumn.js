25b31b5ed4582456ce1fa92b2a60fcd4ba33df5c
joezhouoxford <joezhou@spengineeringltd.com>
2016-05-05 22:00:26 +0100

    for multiple lists

--- CloneColumn.js
+++ CloneColumn.js
@@ -3,36 +3,41 @@
  */
 
 var sourceListTitle="Importtest5";
-var targetListTitle="ImportTest1";
+var targetListTitleList="ImportTest1";
 var sourceFieldInternalName="Datum";
 
 
+//noinspection JSUnresolvedFunction
 var clientContext = new SP.ClientContext.get_current();
 //get web
 var oWebsite = clientContext.get_web();
 
 //get list
-this.oList = oWebsite.get_lists()
+this.oList = oWebsite.get_lists();
 var sourceList=oList.getByTitle(sourceListTitle);
 
 
 //read source list field XML
  // Getting All Fields From the List
-var oFields=sourceList.get_fields()
+var oFields=sourceList.get_fields();
 this.sourceField=oFields.getByInternalNameOrTitle(sourceFieldInternalName);
 clientContext.load(sourceField);
 clientContext.executeQueryAsync(Function.createDelegate(this, this.onGetFieldSuccess));
 
 function onGetFieldSuccess(){
     var sourceFieldXML=this.sourceField.get_schemaXml();
-
+    var targetListTitleArray=targetListTitleList.split(',');
     console.log(sourceFieldXML);
-    addListFieldBySchemaXML(sourceFieldXML);
-};
+    for (var i = 0; i < targetListTitleArray.length; i++) {
+        addListFieldBySchemaXML(targetListTitleArray[i],sourceFieldXML);
+    }
 
-function addListFieldBySchemaXML(schemaXML)
+}
+
+function addListFieldBySchemaXML(targetListTitle,schemaXML)
 {
     var targetList=oList.getByTitle(targetListTitle);
+    console.log("for list" +targetListTitle);
     var fieldValue = targetList.get_fields().addFieldAsXml(schemaXML, false, SP.AddFieldOptions.addToNoContentType);
     fieldValue.update();
     clientContext.load(fieldValue );
